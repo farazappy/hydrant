a36d3baaff94ecb581c0fbda55e90427cf749f15
supriyo15998 <supriyo15998@gmail.com>
2019-10-01 23:13:55 +0530

    new migration

---
+++ database/migrations/2019_10_01_174205_areas.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Areas extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('areas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('area_name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
